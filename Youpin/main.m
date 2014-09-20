d20bf175d64a297372945a27efeebb5cbcd0209e
Sophia Anopa <sanopa@gmail.com>
2014-09-20 00:48:21 -0400

    Initial commit

---
+++ Youpin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Youpin
+//
+//  Created by Sophia Anopa on 20.09.14.
+//  Copyright (c) 2014 Aiiyoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YOHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YOHAppDelegate class]));
+    }
+}
